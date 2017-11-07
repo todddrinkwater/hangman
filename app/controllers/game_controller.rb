a147d8ecf9848e2d48428257bfbdd3fbbe9c75ac
todddrinkwater <todddrinkwaternz@gmail.com>
2017-11-08 10:45:39 +1300

    Create clue and lives_remaining vars for UI
    All tests pass
    Clue only currently displays the word as all masked.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,9 @@
 class GameController < ApplicationController
-  def new
+  def index
+    @game = Game.new(word: "powershop", lives_remaining: 7)
+    @game.save
+    
+    clue_arr = @game.word.chars.map { |char| char = "_" }
+    @clue = clue_arr.join(" ")
   end
 end
--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,15 +1,11 @@
 When(/^I visit the game page$/) do
-  visit new_game_path
+  visit game_index_path
 end
 
 Then(/^I should be shown the clue$/) do
-  expect(page).to have_content("_ _ _ _")
+  expect(page).to have_content("_ _ _ _ _ _ _ _ _")
 end
 
-# Then(/^I should be asked if I am ready to start$/) do
-#   expect(page).to have_content("Ready to play?")
-# end
-
-# Then(/^I should see a start game button$/) do
-#   expect(page).to have_link("Start Game", href: new_game_path)
-# end
+Then(/^I should see the number of lives I have left$/) do
+  expect(page).to have_content("Lives remaining: 7")
+end
