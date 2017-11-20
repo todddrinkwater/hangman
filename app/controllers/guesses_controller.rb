294760b7451a37abec4c3f9d7b6dd58d898b08f4
todddrinkwater <todddrinkwaternz@gmail.com>
2017-11-21 11:59:50 +1300

    add all tests to GamesController

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,21 +1,19 @@
 class GuessesController < ApplicationController
-  
-  # def new
-  #   set up vars that view needs
-  #   @guess = Game.guesses.build
-  #   would have to set this up in new, edit, create
-  # end
-  
-  def create
-    game = Game.find(params[:game_id])
-    new_guess = game.guesses.new(guess_params)
-    new_guess.guess.upcase!
+  def index
+    @game = Game.find(params[:game_id])
+    redirect_to game_path(@game)
+  end
 
-    if new_guess.save
-      redirect_to game_path(game)
+  def create
+    @game = Game.find(params[:game_id])
+    @guess = @game.guesses.new(guess_params)
+    @guess.guess.upcase!
+
+    if @guess.save
+      redirect_to game_path(@game)
     else
-      flash[:notice] = new_guess.errors.full_messages
-      redirect_to game_path(game) #TODO: make this render 'new' view on fail, not redirect.
+      flash[:notice] = @guess.errors.full_messages
+      render 'games/show'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :games, only: [:index, :new, :create, :show] do
-    resources :guesses, only: [:create]
+    resources :guesses, only: [:create, :new, :index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'games#index'
